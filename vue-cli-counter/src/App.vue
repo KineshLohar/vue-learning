61468e2d7a7cd3e0ba1df427bc3649fe5b844999
kritikamalviya <kritikamalviya123@gmail.com>
2026-02-11 17:37:17 +0530

    Initialize Vue 3 project with Vite, including essential configurations, ESLint, Prettier, and a basic counter component.

---
+++ vue-cli-counter/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+
+
+const counter = ref(0);
+
+const inc = () => counter.value++
+
+
+const dec = () => counter.value--
+</script>
+
+<template>
+  <h1>Counter : {{counter}}</h1>
+
+  <button @click="inc">Increment</button>
+  <button @click="dec">Decrement</button>
+</template>
+
+<style scoped></style>
