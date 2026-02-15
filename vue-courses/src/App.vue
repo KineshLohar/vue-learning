fb4292d85061b95620cc710f039e0e585f5022ea
kritikamalviya <kritikamalviya123@gmail.com>
2026-02-15 16:33:00 +0530

    Updated

---
+++ vue-courses/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import CourseItem from './components/CourseItem.vue';
+
+</script>
+
+<template>
+  <div class="p-4 space-y-4">
+    <h1 class="text-4xl">Courses</h1>
+
+    <h2 class="text-2xl">All Courses</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      <CourseItem v-for="i in 4" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ vue-courses/src/components/CourseItem.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="bg-white rounded border border-gray-200">
+        <h3 class="border-b border-gray-200 p-4 font-medium ">Title</h3>
+        <p class="border-b border-gray-200 p-4 font-medium ">Price</p>
+        <p class="p-4">Descriptio</p>
+    </div>
+</template>
